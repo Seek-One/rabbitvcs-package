bbbb6a3bc1b7936c23fdce7146a4b0d279304d50
Eric Beuque <eric.beuque@jet1oeil.com>
2020-10-06 11:07:26 +0200

    Change package name

--- build-pkg.sh
+++ build-pkg.sh
@@ -1,11 +1,15 @@
 #/bin/bash
 
-cd rabbitvcs-git
-python3 setup.py install --install-layout=deb --prefix=../rabbitvcs-debian/usr
+SRC_DIR=./rabbitvcs-git
+PKG_DIR=./rabbitvcs-nemo-pkg
+PKG_PREFIX=../rabbitvcs-nemo-pkg
+PKG_PREFIX_SAFE=\\.\\.\\/rabbitvcs\\-nemo\\-pkg
+
+cd ${SRC_DIR}
+python3 setup.py install --install-layout=deb --prefix=${PKG_PREFIX}/usr
 cd ..
-cp -r DEBIAN/ rabbitvcs-debian/
-mkdir -p rabbitvcs-debian/usr/share/nemo-python/extensions
-cp RabbitVCS.py rabbitvcs-debian/usr/share/nemo-python/extensions
-#cp ./buildinfo.py rabbitvcs-debian/usr/lib/python3/dist-packages/rabbitvcs
-sed -i 's/\.\.\/rabbitvcs\-debian//g' rabbitvcs-debian/usr/lib/python3/dist-packages/rabbitvcs/buildinfo.py
-dpkg-deb --build rabbitvcs-debian/
+cp -r DEBIAN/ ${PKG_DIR}
+mkdir -p ${PKG_DIR}/usr/share/nemo-python/extensions
+cp RabbitVCS.py ${PKG_DIR}/usr/share/nemo-python/extensions
+sed -i "s/${PKG_PREFIX_SAFE}//g" ${PKG_DIR}/usr/lib/python3/dist-packages/rabbitvcs/buildinfo.py
+dpkg-deb --build ${PKG_DIR}/
