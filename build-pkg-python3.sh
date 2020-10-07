b19d7b8414e76fcf9098cd145eb9685826b5da40
Eric Beuque <eric.beuque@jet1oeil.com>
2020-10-07 11:21:53 +0200

    Better script

--- build-pkg-python3.sh
+++ build-pkg-python3.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-PYTHON=python2.7
+PYTHON=python3
 SRC_DIR=./rabbitvcs-git
 PKG_DIR=./rabbitvcs-nemo-${PYTHON}
 PKG_PREFIX=../rabbitvcs-nemo-${PYTHON}
-PKG_PREFIX_SAFE=\\.\\.\\/rabbitvcs\\-nemo\\-python2\\.7
+PKG_PREFIX_SAFE=\\.\\.\\/rabbitvcs\\-nemo\\-${PYTHON}
 
 # Clear old file
 rm -rf ${PKG_DIR}
@@ -15,8 +15,8 @@ cd ${SRC_DIR}
 ${PYTHON} setup.py install --install-layout=deb --prefix=${PKG_PREFIX}/usr
 cd ..
 sed -i "s/${PKG_PREFIX_SAFE}//g" ${PKG_DIR}/usr/lib/${PYTHON}/dist-packages/rabbitvcs/buildinfo.py
-find ${PKG_DIR} -name "*.pyc" -exec rm -f {} \;
-${PYTHON} -m compileall ${PKG_DIR}/usr/lib/${PYTHON}/dist-packages
+
+
 
 # Extensions
 mkdir -p ${PKG_DIR}/usr/share/nemo-python/extensions
@@ -24,5 +24,5 @@ cp RabbitVCS.py ${PKG_DIR}/usr/share/nemo-python/extensions
 
 # Package
 cp -r DEBIAN/ ${PKG_DIR}
-sed -i "s/python3/python/g" ${PKG_DIR}/DEBIAN/control
+
 dpkg-deb --build ${PKG_DIR}/
